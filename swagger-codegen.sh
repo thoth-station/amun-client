667c2fdfa7f565e870fc181f3ea9d229718f3e72
Fridolin Pokorny <fridolin@redhat.com>
2018-10-09 14:48:25 +0200

    Add files needed for generaton

---
+++ swagger-codegen.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# Generate swagger client from Amun's swagger specification.
+# Fridolin Pokorny <fridolin.pokorny@gmail.com>
+#
+# This file automates generation of the swagger client and makes it reproducible. Run this
+# file to automatically generate Python swagger client for interacting with Amun.
+
+set -ex
+
+AMUN_SWAGGER_YAML=${1:-'https://raw.githubusercontent.com/thoth-station/amun-api/master/amun/swagger.yaml'}
+
+function die() {
+    echo $@ 1>&2
+    exit 1
+}
+
+which which > /dev/null || die "Please install which command to continue"
+which git > /dev/null   || die "Please install git to continue"
+which mvn > /dev/null   || die "Please install maven to continue"
+which java > /dev/null  || die "Please install java to continue"
+which find > /dev/null  || die "Please install find utility to continue"
+
+
+if [ ! -d 'swagger-codegen' ]; then
+    git clone https://github.com/swagger-api/swagger-codegen
+    pushd swagger-codegen
+    mvn clean package
+    popd
+fi
+
+
+rm -rf swagger-codegen-output/
+java -jar swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+    -i "${AMUN_SWAGGER_YAML}" \
+    -l python \
+    -o swagger-codegen-output/ \
+    -c swagger-codegen.json
+
+rm -rf amun/swagger_client
+cp -r swagger-codegen-output/amun/swagger_client/ amun/swagger_client
+# There is a bug in swagger-codegen - it does not respect sub-package for some files, this is a simple workaround.
+find swagger-codegen-output/amun.swagger_client/ -iname '*.py' -exec sed -i 's/^from amun/from amun.swagger_client.amun/' {} \+
+cp -r swagger-codegen-output/amun.swagger_client/* amun/swagger_client
+cp -r swagger-codegen-output/docs Documentation
+
+# Nullable values are not recognized by swagger-codegen.sh.
+sed -i '/.*if container is None:/,+1 d' "amun/swagger_client/models/inspection_job_status_response_status.py"
+sed -i '/.*if exit_code is None:/,+1 d' "amun/swagger_client/models/inspection_job_status_response_status.py"
+sed -i '/.*if finished_at is None:/,+1 d' "amun/swagger_client/models/inspection_job_status_response_status.py"
+sed -i '/.*if reason is None:/,+1 d' "amun/swagger_client/models/inspection_job_status_response_status.py"
+sed -i '/.*if started_at is None:/,+1 d' "amun/swagger_client/models/inspection_job_status_response_status.py"
+
+# TODO: we should create separate models for Build and Job
+# for now this is ok, but keep this in mind :)
+#sed -i '/.*if container is None:/,+1 d' "amun/swagger_client/models/inspection_build_status_response_status.py"
+#sed -i '/.*if exit_code is None:/,+1 d' "amun/swagger_client/models/inspection_build_status_response_status.py"
+#sed -i '/.*if finished_at is None:/,+1 d' "amun/swagger_client/models/inspection_build_status_response_status.py"
+#sed -i '/.*if reason is None:/,+1 d' "amun/swagger_client/models/inspection_build_status_response_status.py"
+#sed -i '/.*if started_at is None:/,+1 d' "amun/swagger_client/models/inspection_build_status_response_status.py"
