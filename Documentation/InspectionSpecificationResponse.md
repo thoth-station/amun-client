80345a32ba668e184e329bbae97338372d182aa6
Fridolin Pokorny <fridolin@redhat.com>
2018-11-19 14:56:58 +0100

    Add hardware to requests

---
+++ Documentation/InspectionSpecificationResponse.md
@@ -0,0 +1,11 @@
+# InspectionSpecificationResponse
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**parameters** | **object** | Parameters echoed back to user (with default parameters if omitted).  | 
+**specification** | [**InspectionSpecificationResponseSpecification**](InspectionSpecificationResponseSpecification.md) |  | 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- amun/swagger_client/amun/inspection_api.py
+++ amun/swagger_client/amun/inspection_api.py
@@ -227,6 +227,103 @@ class InspectionApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def get_inspection_specification(self, inspection_id, **kwargs):  # noqa: E501
+        """Get specification of the given inspection.  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_inspection_specification(inspection_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str inspection_id: Id of inspection run. (required)
+        :return: InspectionSpecificationResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.get_inspection_specification_with_http_info(inspection_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.get_inspection_specification_with_http_info(inspection_id, **kwargs)  # noqa: E501
+            return data
+
+    def get_inspection_specification_with_http_info(self, inspection_id, **kwargs):  # noqa: E501
+        """Get specification of the given inspection.  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_inspection_specification_with_http_info(inspection_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str inspection_id: Id of inspection run. (required)
+        :return: InspectionSpecificationResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['inspection_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_inspection_specification" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'inspection_id' is set
+        if ('inspection_id' not in params or
+                params['inspection_id'] is None):
+            raise ValueError("Missing the required parameter `inspection_id` when calling `get_inspection_specification`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'inspection_id' in params:
+            path_params['inspection_id'] = params['inspection_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = []  # noqa: E501
+
+        return self.api_client.call_api(
+            '/inspect/{inspection_id}/specification', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='InspectionSpecificationResponse',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def get_inspection_status(self, inspection_id, **kwargs):  # noqa: E501
         """Get status of an inspection.  # noqa: E501
 
